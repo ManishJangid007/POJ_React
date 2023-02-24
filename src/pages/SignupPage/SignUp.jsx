670a239392c78bb2da8ca92e3816a3cb91099611
ManishJangid007 <87350002+ManishJangid007@users.noreply.github.com>
2023-02-24 12:08:09 +0530

    Signup form fixed

--- src/pages/SignupPage/SignUp.jsx
+++ src/pages/SignupPage/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Background from "../../components/Background/Background";
 import BackIco from "../../assets/icons/back_white.png";
 import POJIco from "../../assets/icons/poj.png";
@@ -7,6 +7,30 @@ import "./SignUp.css";
 
 export default function SignUp() {
     const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        firstname: "",
+        lastname: "",
+        gender: "",
+        dob: "",
+        email: "",
+        username: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    function handelChange(event) {
+        setFormData(oldFormData => ({
+            ...oldFormData,
+            [event.target.name]: event.target.value
+        }))
+    }
+
+    function handelSubmit(event) {
+        event.preventDefault();
+        console.log(formData);
+    }
+
     return (
         <div className="signupage">
             <Background />
@@ -15,31 +39,117 @@ export default function SignUp() {
                     <img src={BackIco} alt="close icon" onClick={() => navigate(-1)} />
                 </div>
                 <div className="signup--form">
+
                     <img src={POJIco} alt="poj icon" />
                     <h1>Sign Up</h1>
-                    <div className="signup--inputgrid">
-                        <div className="signup--inputdiv">
-                            <input type="text" placeholder="Firstname" />
+
+                    <form onSubmit={(e) => handelSubmit(e)}>
+
+                        <div className="signup--inputgrid">
+
+                            <div className="signup--fielddiv">
+                                <label htmlFor="firstname">Firstname</label>
+                                <div className="signup--inputdiv">
+                                    <input
+                                        type="text"
+                                        name="firstname"
+                                        placeholder="Enter firstname..."
+                                        onChange={(e) => handelChange(e)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="signup--fielddiv">
+                                <label htmlFor="lastname">Lastname</label>
+                                <div className="signup--inputdiv">
+                                    <input
+                                        type="text"
+                                        name="lastname"
+                                        placeholder="Enter lastname..."
+                                        onChange={(e) => handelChange(e)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="signup--fielddiv">
+                                <label htmlFor="gender">Gender</label>
+                                <div className="signup--inputdiv">
+                                    <select
+                                        name="gender"
+                                        onChange={(e) => handelChange(e)}
+                                    >
+                                        <option value="select">Select</option>
+                                        <option value="male">Male</option>
+                                        <option value="female">Female</option>
+                                        <option value="transgender">Transgender</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className="signup--fielddiv">
+                                <label htmlFor="dob">Date of Birth</label>
+                                <div className="signup--inputdiv">
+                                    <input
+                                        type="date"
+                                        name="dob"
+                                        onChange={(e) => handelChange(e)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="signup--fielddiv">
+                                <label htmlFor="email">Email</label>
+                                <div className="signup--inputdiv">
+                                    <input
+                                        type="Email"
+                                        name="email"
+                                        placeholder="Enter email..."
+                                        onChange={(e) => handelChange(e)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="signup--fielddiv">
+                                <label htmlFor="username">Username</label>
+                                <div className="signup--inputdiv">
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        placeholder="Choose username..."
+                                        onChange={(e) => handelChange(e)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="signup--fielddiv">
+                                <label htmlFor="password">Password</label>
+                                <div className="signup--inputdiv">
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        placeholder="Choose password..."
+                                        onChange={(e) => handelChange(e)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="signup--fielddiv">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <div className="signup--inputdiv">
+                                    <input
+                                        type="password"
+                                        name="confirmPassword"
+                                        placeholder="Confirm Password"
+                                        onChange={(e) => handelChange(e)}
+                                    />
+                                </div>
+                            </div>
+
                         </div>
-                        <div className="signup--inputdiv">
-                            <input type="text" placeholder="Lastname" />
-                        </div>
-                        <div className="signup--inputdiv">
-                            <input type="Email" placeholder="Email" />
-                        </div>
-                        <div className="signup--inputdiv">
-                            <input type="text" placeholder="Username" />
-                        </div>
-                        <div className="signup--inputdiv">
-                            <input type="password" placeholder="Password" />
-                        </div>
-                        <div className="signup--inputdiv">
-                            <input type="password" placeholder="Confirm Password" />
-                        </div>
-                    </div>
-                    <div className="signup--loginbtn">
-                        <h3>Sign Up</h3>
-                    </div>
+                        <button className="signup--loginbtn">Sign Up</button>
+
+                    </form>
+
                     <div className="signup--bottom--text">
                         <h4>Already Have a Account ? <span onClick={() => navigate("/login")}>Login</span></h4>
                     </div>
