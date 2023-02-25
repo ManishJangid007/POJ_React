c2a88f7988736881c202ed8b929fcfdcbcf93a69
ManishJangid007 <87350002+ManishJangid007@users.noreply.github.com>
2023-02-26 00:42:06 +0530

    Getting users name on profile ico component

--- src/components/ProfileIco/ProfileIco.jsx
+++ src/components/ProfileIco/ProfileIco.jsx
@@ -2,15 +2,46 @@ import React from "react";
 import "./ProfileIco.css";
 import PersonIco from "../../assets/icons/person.png"
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 
 export default function ProfileIco() {
     const navigate = useNavigate();
+
+    const { isLoading, error, data } = useQuery('userData', () =>
+        axios.get('/api/user')
+            .then(res => res.data)
+    );
+
+    async function logoutUser() {
+        try {
+            const res = await axios.get("/api/user/logout");
+            window.location.reload(false);
+            return res;
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
     return (
         <div className="profileico">
             <div className="header--user">
-                <h3>Username</h3>
-                <h4 onClick={() => navigate("/login")}>Login</h4>
+                {
+                    isLoading ?
+                        <h3>Getting...</h3> :
+                        error ? <h3>.|.|.</h3> :
+                            data.isAuthenticated ?
+                                <>
+                                    <h3>{data.fullName}</h3>
+                                    <h4 onClick={logoutUser}>Logout</h4>
+                                </>
+                                :
+                                <>
+                                    <h3>Guest User</h3>
+                                    <h4 onClick={() => navigate("/login")}>Login</h4>
+                                </>
+                }
+
             </div>
             <div className="header--profile">
                 <img src={PersonIco} alt="person icon" />
