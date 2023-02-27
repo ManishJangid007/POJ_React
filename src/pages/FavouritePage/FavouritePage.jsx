8a05ba311150bffb9ffa62c7836a69a6f1e5f09c
ManishJangid007 <87350002+ManishJangid007@users.noreply.github.com>
2023-02-27 20:25:52 +0530

    new : loged out user message

--- src/pages/FavouritePage/FavouritePage.jsx
+++ src/pages/FavouritePage/FavouritePage.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DishCard from '../../components/DishCard/Dishcard';
+import axios from 'axios';
+import Loading from '../../components/Loading/Loading';
 import "../HomePage/HomePage.css";
+import "./FavouritePage.css";
 
 
 export default function FavouritePage() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const recipes = [];
+
+    useEffect(() => {
+        axios.get('/api/user/')
+            .then(res => setIsAuthenticated(res.data.isAuthenticated))
+    }, []);
+
     return (
-        <div className="custom--grid">
-            <DishCard />
-            <DishCard />
-            <DishCard />
-            <DishCard />
-        </div>
+        isAuthenticated ?
+            <div className="custom--grid">
+                <h3>Recipes Goes Here</h3>
+            </div> :
+            <div className='fav--errmsg--div'>
+                <h3>You Still Loged out 😆, Login ! 👉 </h3>
+                <a href='/login'>Login</a>
+            </div>
     )
 }
